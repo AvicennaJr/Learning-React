c49e4acfdfbe75a6b6aa67378cb69ddb8a048ce2
AvicennaJr <justfuad@protonmail.com>
2022-11-07 01:42:13 +0300

    First react app

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import * as React from 'react'
+
+
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
+
+
+function App() {
+    return (
+        <div>
+            <h1>My hacker stories</h1>
+            <Search />
+            <hr />
+            <List />
+        </div>
+    );
+}
+
+function List() {
+    return (
+        <ul>
+            {list.map(function (item) {
+                return (
+                    <li key={item.objectID}>
+                        <span>
+                            <a href={item.url}>{item.title}</a>
+                        </span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+}
+
+function Search() {
+    return (
+        <div>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" />
+        </div>
+    );
+}
+export default App;
